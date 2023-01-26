fec844dac069a039645b29bbcfee8a96c33f3e86
Alan.Chu <achu61@seas.upenn.edu>
2023-01-26 10:47:08 +0800

    DB connected

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require("express");
+const mongoose = require("mongoose");
 
 //initial the application
 const app = express();
@@ -7,6 +8,16 @@ const app = express();
 // config setting
 require("dotenv").config();
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// establish connection to DB
+mongoose.set("strictQuery", true);
+mongoose.connect(DATABASE_URL);
+
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + " is mongo not running?"));
+db.on("connected", () => console.log("mongoDB connected"));
+// routes
 
 //Listerning
 app.listen(PORT, () => {
