6574864a05a5c8ffc2ad74c32d44c04a2d6a430a
Alan Chu <achu61@seas.upenn.edu>
2023-09-10 22:59:58 -0700

    updated Alan info

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 module.exports = [
   {
     name: "Alan Chu",
-    highestDegree: "MCIT at UPenn",
+    highestDegree: "美国普渡大学材料工程学系博士候选人/美国宾夕法尼亚大学计算机和信息技术硕士在读",
     img: "https://res.cloudinary.com/dnn0tzpjf/image/upload/v1674702525/Alan01.png",
     subjects: "math, physics, computer science",
-    description: "Here is the description for Alan",
+    description: "我司的CEO，曲华德老师本硕毕业于国立清华大学化学工程专业，后成为普渡大学材料工程学系的博士候选人。他在硅谷担任过半导体工程师，并继续攻读宾夕法尼亚大学计算机和信息技术硕士学位，来持续不断地丰富自己的学识经历。他担任过Lafayette Regional Science Fair的科研评委，有两篇论文被美国知名期刊收录，多篇会议论文获奖。在教学过程中，他坚持从基础入手，以稳固的基础助学生逐步提升成绩。擅长发现学生的学习痛点，找到学习逻辑链，并用全局的学术视角推动学生全面提升。由于理科和工科知识面广泛，教学经验丰富，能更好地鼓励学生，因材施教，引导他们发现自身优势和规划未来深造方向。\n他辅导过的学生，在剑桥大学的经济学入学评估考试（ECAA）中取得高分，在Edexcel数学/进阶数学、CIE数学等皆获得了A*和A的优异成绩。他的指导进一步地帮助了学生们斩获墨尔本大学、悉尼大学、曼彻斯特大学、伦敦大学学院等顶尖院校的入学通知书。他辅导过的学生，在剑桥大学的经济学入学评估考试（ECAA）中取得高分，在Edexcel数学/进阶数学、CIE数学等皆获得了A*和A的优异成绩。他的指导进一步地帮助了学生们斩获墨尔本大学、悉尼大学、曼彻斯特大学、伦敦大学学院等顶尖院校的入学通知书。",
   },
   {
     name: "Y. Shi",
